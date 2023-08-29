5a01bc29c0c371edbf7e6accf16181e6299653ee
sherifa-aljutaili <142884985+sherifa-aljutaili@users.noreply.github.com>
2023-08-29 16:21:41 +0300

    sherifa`s class work 3

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-# write your code here
+
+#pt1 
+Favorite_animals = ['Dog', 'cat', 'monkey', 'rabbit']
+print (Favorite_animals)
+
+print (Favorite_animals[1])
+
+Favorite_animals.remove('monkey')
+#or Favorite_animals.pop(2)
+print (Favorite_animals)
+
+print('_________________________________________')
+
+#pt2
+Favorite_animals.append('duck')
+print (Favorite_animals)
+
+for Favorite_animal in Favorite_animals : 
+    print(f'-I love {Favorite_animal}s')
+
+print('_________________________________________')
+
+#pt3
+numbers = [1, 2, 3, 4, 5]
+numbers_sum = 0 
+
+for number in numbers : 
+    numbers_sum += number
+
+print(numbers_sum) 
+
+    
+
+
